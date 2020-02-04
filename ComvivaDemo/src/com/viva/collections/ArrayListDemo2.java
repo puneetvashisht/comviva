872fbd8ce66493a1ac1b89d91ac2c403279acd6a
puneetvashisht@gmail.com <puneetvashsiht@gmail.com>
2020-02-04 10:48:15 +0530

    Sets

--- ComvivaDemo/src/com/viva/collections/ArrayListDemo2.java
+++ ComvivaDemo/src/com/viva/collections/ArrayListDemo2.java
@@ -1,6 +1,10 @@
 package com.viva.collections;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.viva.Employee;
 
 
 class Fruit{
@@ -28,20 +32,47 @@ class Fruit{
 public class ArrayListDemo2 {
 
 	public static void main(String[] args) {
-		ArrayList<Fruit> fruits = new ArrayList<>();
-		Fruit f1 = new Fruit("Banana");
+		Set<String> fruits = new HashSet<>();
+		String f1 = "Banana";
 		
-		fruits.add(new Fruit("Apples"));
-		fruits.add(new Fruit("Mango"));
+		fruits.add("Apples");
+		fruits.add("Mango");
 		fruits.add(f1);
 		
 		System.out.println(fruits);
 		
 		
-		Fruit f2 = new Fruit("Banana");
+		String f2 = "Banana";
+		
+		fruits.add(f2);
 		System.out.println(f1.equals(f2));
 		
-		System.out.println(fruits.contains(f2));
+		System.out.println(fruits.size());
+		
+		
+		
+		Set<Employee> employees = new HashSet<>();
+		Employee e1 = new Employee(1, "Ravi", 34344.34);
+		Employee e2 = new Employee(2, "Priya", 44344.34);
+		Employee e3 = new Employee(3, "Amit", 34344.34);
+		Employee e4 = new Employee(12, "Sameer", 44344.34);
+		employees.add(e1);
+		employees.add(e2);
+		employees.add(e3);
+		employees.add(e4);
+		Employee searchForThisEmployee = new Employee(3, "Amit", 34344.34);
+		employees.add(searchForThisEmployee);
+		
+//		employees.add(2, e4);
+		
+		
+		System.out.println(employees.size());
+		
+		
+		boolean result = employees.contains(searchForThisEmployee);
+		System.out.println(result);
+		
+		
 
 	}
 
